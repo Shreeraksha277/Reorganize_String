5922069f540e733a818411f1e852467d65ede29c
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-05-22 19:17:31 +0530

    Create sol.java

---
+++ sol.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public String reorganizeString(String s) {
+        int n = s.length();
+        int[] freq = new int[26];
+
+        // count frequency
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        // max heap based on frequency
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> b[1] - a[1]
+        );
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                if (freq[i] > (n + 1) / 2) return "";
+                pq.add(new int[]{i, freq[i]});
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (pq.size() > 1) {
+            int[] first = pq.poll();
+            int[] second = pq.poll();
+
+            sb.append((char)(first[0] + 'a'));
+            sb.append((char)(second[0] + 'a'));
+
+            if (--first[1] > 0) pq.add(first);
+            if (--second[1] > 0) pq.add(second);
+        }
+
+        if (!pq.isEmpty()) {
+            sb.append((char)(pq.poll()[0] + 'a'));
+        }
+
+        return sb.toString();
+    }
+}
